4eb886ae9f4bf571d94c419f73d2424fb8c35bc4
Brian Lopez <lopezbrianmisael@gmail.com>
2024-07-06 12:29:03 -0300

    [MOD] changen name for previus field 'deposit' to 'warehouse_id'

--- models/sales_channels.py
+++ models/sales_channels.py
@@ -5,5 +5,5 @@ class SaleChannel(models.Model):
 
     name = fields.Char(string=_("Name"), required=True)
     code = fields.Char(string=_("Code"), default=lambda self: self.env['ir.sequence'].next_by_code('sale.channel'), readonly=True)
-    deposit = fields.Many2one(comodel_name="stock.warehouse", string=_("Deposit"))
+    warehouse_id = fields.Many2one(comodel_name="stock.warehouse", string=_("Deposit"))
     invoice_journal_id = fields.Many2one(comodel_name="account.journal", string=_("Invoice Journal"))
