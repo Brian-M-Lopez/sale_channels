b00efc2073c9ceed7bcacda696c5968e66809056
Brian Lopez <lopezbrianmisael@gmail.com>
2024-07-06 14:05:09 -0300

    [MOD] Changed sale_channel_id, now it has string property for translations

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api, _
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    sale_channel_id = fields.Many2one(comodel_name="sale.channel", related="sale_id.sale_channel_id")
+    sale_channel_id = fields.Many2one(comodel_name="sale.channel", string=_("Sale Channel"), related="sale_id.sale_channel_id")
