c5676f3c268ad26a7ad9f1b9ad7f726f1f21aafc
Brian Lopez <lopezbrianmisael@gmail.com>
2024-07-06 13:26:37 -0300

    [ADD]Added onchange method, now the warehouse_id is changed based on sale_channel_id

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,7 +1,12 @@
-from odoo import models, fields, _
+from odoo import models, fields, api, _
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
     sale_channel_id = fields.Many2one(comodel_name="sale.channel", string=_("Sale Channel"), required=True)
+
+    @api.onchange("sale_channel_id")
+    def _oncahnge_sale_channel_id(self):
+        if self.sale_channel_id and self.sale_channel_id.warehouse_id:
+            self.warehouse_id = self.sale_channel_id.warehouse_id
